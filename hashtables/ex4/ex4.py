d270c579afa02a8504bc73af302d70a0736fecfd
Virginia Davenport <virginialdport@gmail.com>
2020-09-04 09:31:47 -0600

    ex4. complete and passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,22 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    #make a dictionary to cache matches return the keys
+    #add all numbers to the dict as key and value
+    #check to see if a opposite match exists
+    # add matches to output list 
+    
+    int_dict = {}
+    output = []
+    for i in a:
+        int_dict[i] = i #populate the dict
 
-    return result
+        if i != 0 and - i in int_dict: #check if a negative counterpart is there
+            output.append(abs(i))
+
+
+
+    return output
 
 
 if __name__ == "__main__":
